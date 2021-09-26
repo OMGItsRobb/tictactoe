e84ff447098291979a754a596517cb1859cee5f5
Nolan <ntronowicz@gmail.com>
2021-09-25 23:31:52 -0400

    Added board logics and states

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -2,26 +2,51 @@ import React, { useState } from 'react';
 import Square from './Square';
 
 const board = () => {
-    const [status, setBoard] = useState(Array(9).fill(null));
+    const [board, setBoard] = useState(Array(9).fill(null));
+    const [isXNext, setIsXNext] = useState(false);
 
-    console.log(status);
+    const handleSquareClick = position => {
+        if (board[position]) {
+            return;
+        }
+        setBoard(previous => {
+            return previous.map((square, pos) => {
+                if (pos === position) {
+                    return isXNext ? 'X' : 'O';
+                }
+                return square;
+            });
+        });
+        setIsXNext(prev => !prev);
+    };
+
+    const renderSquare = position => {
+        return (
+            <Square
+                value={board[position]}
+                onClick={() => handleSquareClick(position)}
+            />
+        );
+    };
+
+    console.log(board);
 
     return (
         <div className="board">
             <div className="board-row">
-                <Square value={0} />
-                <Square value={1} />
-                <Square value={2} />
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
             </div>
             <div className="board-row">
-                <Square value={3} />
-                <Square value={4} />
-                <Square value={5} />
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
             </div>
             <div className="board-row">
-                <Square value={6} />
-                <Square value={7} />
-                <Square value={8} />
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
             </div>
         </div>
     );
